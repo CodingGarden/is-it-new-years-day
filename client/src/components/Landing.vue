ab32088beee742b5bd4081d277425e07b6b697ad
w3cj <cj@null.computer>
2019-12-29 09:54:18 -0700

    :bug: ignore console.error

--- client/src/components/Landing.vue
+++ client/src/components/Landing.vue
@@ -24,6 +24,7 @@ export default {
   setup() {
     const socket = io(API_URL);
     socket.on('error', (message) => {
+      // eslint-disable-next-line
       console.error(message);
     });
     return {

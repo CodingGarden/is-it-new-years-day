7a2da54e71f1bb43d07fd9d853333507f1826829
w3cj <cj@null.computer>
2019-12-28 18:06:11 -0700

    :sparkles: un-do load test

--- server/src/sockets.js
+++ server/src/sockets.js
@@ -6,11 +6,12 @@ module.exports = (server) => {
   const state = {};
 
   io.on('connection', (socket) => {
-    console.log('Total connections:', Object.keys(state).length);
     console.log('a user connected', socket.id);
+    console.log('Total connections:', Object.keys(state).length);
     socket.on('location', (location) => {
+      if (location.x < 0 || location.x > 1) return;
+      if (location.y < 0 || location.y > 1) return;
       state[socket.id] = location;
-      io.emit('state', state);
     });
     socket.on('disconnect', () => {
       console.log('a user disconnected', socket.id);
@@ -18,4 +19,8 @@ module.exports = (server) => {
       delete state[socket.id];
     });
   });
+
+  setInterval(() => {
+    io.emit('state', state);
+  }, 500);
 };

d69219b146b060654e63fa8bb589e1892d5b46a1
w3cj <cj@null.computer>
2019-12-29 00:36:13 -0700

    :sparkles: remove unused dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,13 @@
 import '@formatjs/intl-relativetimeformat/polyfill-locales';
 import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
+
 import App from './App.vue';
-import store from './store';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueCompositionApi);
 
 new Vue({
-  store,
   render: h => h(App),
 }).$mount('#app');
